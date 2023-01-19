a5e62c46238a588211c371a7a335a3b0346b0782
sqxy17 <115132354+sqxy17@users.noreply.github.com>
2023-01-19 16:43:04 +0800

    Update 冒泡排序.c

--- 冒泡排序.c
+++ 冒泡排序.c
@@ -1,26 +1,39 @@
-//#include<stdio.h>
-//int main()
-//{
-//	int arr[] = { 1,2,354,65,34,5,65,46,346,45,6,35,3 };
-//	int i = 0, p = 0;
-//	int num = sizeof arr / sizeof(arr[0]);
-//	for (i = 0; i < num; i++)
-//	{
-//		int m = 0;
-//		int x = 0;
-//		for (m = 0; m < num; m++)
-//		{
-//			if (arr[i] < arr[m])
-//			{
-//				x= arr[i];
-//				arr[i] = arr[m];
-//				arr[m] = x;
-//			}
-//		}
-//	}
-//	for (p = 0; p < num; p++)
-//	{
-//		printf("%d   ", arr[p]);
-//	}
-//	return 0;
-//}//一串数字有n个元素，需要进行n-1次比较，进行完之后能将最大值或者最小值换到最前面。
+#include<stdio.h>
+
+
+void bubble_sort(int* arr,int sz)  //把小的数字放在前面
+{
+    if(sz>=2)
+    {
+        int i=0;
+        int j=1;
+        int exchange=0;
+        for(i=0;i<=sz-2;i++)
+        {
+            for(j=i+1;j<=sz-1;j++)
+            {
+                if(*(arr+i)>=*(arr+j))
+                {
+                    exchange=*(arr+i);
+                    *(arr+i)=*(arr+j);
+                    *(arr+j)=exchange;
+                }
+            }
+        }
+    }
+    else
+    {
+        printf("数组就一个数你来排序？");
+    }
+}
+
+int main(void)
+{
+    int arr[10]={1,4,7,3,8,6,5,3,45,23};
+    int sz=sizeof(arr)/sizeof (arr[0]);
+    bubble_sort(arr,10);
+    for(int i=0;i<sz;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
